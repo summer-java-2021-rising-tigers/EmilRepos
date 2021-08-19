6309657ba6192b88e2cdaed89f72a8874cc4459e
Emil Nikolov <e.nikolov92@gmail.com>
2021-08-19 14:50:41 +0300

    little fixes

--- APIConnect/src/Servlets/authServlet.java
+++ APIConnect/src/Servlets/authServlet.java
@@ -20,8 +20,17 @@ public class authServlet extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
     	RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("https://www.dnd5eapi.co/api");
+
+
 //    	this.respo = dispatcher.toString();
     	// System.out.println("hahah");
+
+		System.out.println("hahah");
+		System.out.println("hahah1");
+		System.out.println("hahah2");
+		System.out.println("hahah3");
+		System.out.println("hahah4");
+
     	// this.respo = "Ut hendrerit enim ac lectus accumsan, ac elementum magna auctor. Maecenas imperdiet mattis sollicitudin. Phasellus vel nunc eu lorem dignissim commodo vitae vitae nulla. Ut sollicitudin dui vel neque iaculis pulvinar. Praesent lobortis elit mauris, sit amet aliquet orci bibendum in. Cras eget justo id augue feugiat tristique et non turpis. Nunc nec nunc non nisl sagittis mattis.";
     	
 	}
